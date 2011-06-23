8c7119a24077f7497de4d79a0598f42bbf9ed2c2
Tracer Andrew <atracer@princeton.edu>
2011-06-23 10:40:23 -0400

    minor changes

--- CLUSTERING/clust_test.py
+++ CLUSTERING/clust_test.py
@@ -9,13 +9,16 @@ fileCheck1 = re.compile('.bt9')
 fileCheck2 = re.compile('mesh')
 reader = datareader()
 
-Qx = dict() #filename : [vect(Qx), vect(Qy), vect(Qz), vect(H)]
+file_data = dict() #filename : [vect(Qx), vect(Qy), vect(Qz), vect(H)]
 
 for root, dirs, files in os.walk('./FILES/Mar27_2011/'):
 	for fname in files:
 		if fileCheck1.search(fname) and fileCheck2.search(fname):
 			filename = basePath + fname
 			data = reader.readbuffer(filename)
-			Qx[fname] = [data.data['Qx'],data.data['Qy'], data.data['Qz'], data.data['E']]
-print len(Qx)
+			file_data[fname] = [data.data['Qx'],data.data['Qy'], data.data['Qz'], data.data['E']]
+#for i in file_data:
+#	if sum(file_data[i][3]) > 0.0:
+#		print sum(file_data[i][3])
+print len(file_data)
 			
